4dc0bf0600077f10ca64ef7a8eb7046f5c5a0751
abhidatta0 <abhidatta146@gmail.com>
2026-01-05 19:10:15 +0530

    changeSSO text

--- application/src/app/sso-callback.tsx
+++ application/src/app/sso-callback.tsx
@@ -66,7 +66,7 @@ export default function SSOCallback() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
       <ActivityIndicator size="large" color="#000" />
-      <Text style={{ marginTop: 16, color: '#666' }}>SSO callback screen</Text>
+      <Text style={{ marginTop: 16, color: '#666' }}>Loading...</Text>
     </View>
   );
 }
