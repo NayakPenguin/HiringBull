2ececfed9e8103d735fdea7695b169e2baedee6c
Nayaker <nayak.primary@gmail.com>
2026-01-10 18:57:05 +0530

    sync

--- webclient/src/firebase.js
+++ webclient/src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCdbui66J5d9lulSr8to75lS0bLCZTqpZA",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "hiringbull-in.firebaseapp.com",
   projectId: "hiringbull-in",
   storageBucket: "hiringbull-in.firebasestorage.app",
